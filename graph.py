c57052d5ea782c453401e87fa64bd6d4435e2bbe
alessiabadeu <alessiabadeu44@gmail.com>
2026-06-26 10:30:34 +0300

    last version

--- graph.py
+++ graph.py
@@ -1,22 +1,20 @@
 import networkx as nx
-import matplotlib.pyplot as plt #biblioteca de plotare
+import matplotlib.pyplot as plt
 import random
 
-#1. Create graph
 def create_graph():
-    G = nx.Graph() #creearea grafului
-    us_airports = set() #set pentru a retine aeroporturile din US; folosim pentru routes
-
+    G = nx.Graph()
+    us_airports = set()
     with open("airports.dat.txt", "r", encoding="utf-8") as f:
 
         for line in f:
             splited_line = line.strip().split(",")
-            airport_code = splited_line[4].strip().replace('"','') #de ex "GKA" devine GKA (fara ghilimele)
+            airport_code = splited_line[4].strip().replace('"','')
             airport_name = splited_line[1].strip().replace('"','') 
             airport_country = splited_line[3].strip().replace('"', '')
             if airport_country == "United States" and airport_code!="\\N": 
                 G.add_node(airport_code, name=airport_name, country=airport_country)
-                us_airports.add(airport_code) #adaugam aeroportul in set 
+                us_airports.add(airport_code)
 
     with open("routes.dat.txt","r", encoding="utf-8") as f:
         for line in f:
@@ -24,19 +22,15 @@ def create_graph():
             origin = splited_line[2].strip().replace('"', '')
             destination = splited_line[4].strip().replace('"', '')
 
-            if len(splited_line)>=4 and origin in us_airports and destination in us_airports: #ne asiguram ca route e in US
+            if len(splited_line)>=4 and origin in us_airports and destination in us_airports:
                 G.add_edge(origin,destination)
     
     return G
 
-#2. Central betweeness-hubs
-
 def find_hubs(G):
-    centrality = nx.betweenness_centrality(G) #se returneaza un dictionar, cheile sunt nodurile, valorile sunt betweenness centrality
-    hubs = sorted(centrality.items(), key = lambda x: x[1], reverse=True)[:10] #sortam descrescator dupa valoare si pastram doar primele 10 elemente (primele 10 hub-uri)
+    centrality = nx.betweenness_centrality(G)
+    hubs = sorted(centrality.items(), key = lambda x: x[1], reverse=True)[:10]
     return hubs
-
-#3.Attack+Load redistribution
 
 def attack(G, nodes, alpha):
 
@@ -48,28 +42,28 @@ def attack(G, nodes, alpha):
 
     print("Removed nodes:", nodes)
 
-    nodes_over_time = []  #pt grafic
+    nodes = []
     iteration = 0
 
     while True:
         iteration += 1
-        load = nx.betweenness_centrality(G_temp) #!ce e normalized????  #daca stergem un nod, se face redistribuirea si load e recalculat
+        load = nx.betweenness_centrality(G_temp)
 
-        failed = []  #nodurile care au load>capacitatea
+        failed = []
         for n in G_temp.nodes():
             if load[n] > capacity[n]:
                 failed.append(n)
 
-        nodes_over_time.append(G_temp.number_of_nodes()) #pt grafic, retine cate noduri mai sunt la fiecare iteratie
+        nodes.append(G_temp.number_of_nodes()) 
 
-        if not failed:  #daca nu mai cade niciun nod, iesim din while
+        if not failed: 
             break
 
         G_temp.remove_nodes_from(failed)
 
-    nodes_over_time.append(G_temp.number_of_nodes())
+    nodes.append(G_temp.number_of_nodes())
     
-    return G_temp, nodes_over_time
+    return G_temp, nodes
 
 def Sandy(G, alpha):
     return attack(G, ["JFK", "LGA", "EWR"], alpha)
@@ -87,20 +81,20 @@ def analyze_cascade(G, G_temp):
 
     print("Failed airports:",initial_nodes - G_temp.number_of_nodes())
 
-    largest_component = max(nx.connected_components(G_temp),key=len)  #connected_components-lista cu comp conexe, 
+    largest_component = max(nx.connected_components(G_temp),key=len) 
 
     S = len(largest_component)
     print("Largest component size:", S)
-    print("Normalized S:",S / initial_nodes) #cat din reteaua originala se afla in cea mai mare comp conexa 
+    print("Normalized S:",S / initial_nodes) 
 
-    E_before = nx.global_efficiency(G)  #????
+    E_before = nx.global_efficiency(G)
     E_after = nx.global_efficiency(G_temp)
 
     print("Efficiency before:",E_before)
     print("Efficiency after:",E_after)
 
 def plot_cascade(nodes_over_time, nodes):
-    plt.plot(nodes_over_time, marker='o')  #marker pune cerculet in fiecare punct
+    plt.plot(nodes_over_time, marker='o')
 
     plt.xlabel("Iteration")
     plt.ylabel("Remaining nodes")
@@ -123,35 +117,30 @@ def plot_comparison(nodes_targeted, nodes_random, label_targeted, alpha):
     plt.show()
 
 def random_attack(G, alpha):
-    # Set a seed for reproducibilityU, whatever that means girl 
     random_node = random.choice(list(G.nodes()))
     return attack(G, [random_node], alpha)
-
-
 
 def main():
     G =create_graph()
 
     G_temp, nodes_over_time1 = attack(G, ["ANC"], 1.1)
     analyze_cascade(G, G_temp)
-    plot_cascade(nodes_over_time1, ["ANC"])
 
-    # G_temp, nodes_over_time2 = random_attack(G, 1.1)
-    # analyze_cascade(G, G_temp)
-    # plot_cascade(nodes_over_time2, "Random Node")
+    G_temp, nodes_over_time2 = random_attack(G, 1.1)
+    analyze_cascade(G, G_temp)
 
-    # plot_comparison(nodes_over_time1, nodes_over_time2, "DEN", 1.2)
+    plot_comparison(nodes_over_time1, nodes_over_time2, "DEN", 1.2)
 
-    # G_temp, nodes_over_time = Sandy(G, 1.1)
-    # analyze_cascade(G, G_temp)
-    # plot_cascade(nodes_over_time, "Sandy")
+    G_temp, nodes_over_time = Sandy(G, 1.1)
+    analyze_cascade(G, G_temp)
+    plot_cascade(nodes_over_time, "Sandy")
 
-    # G_temp, nodes_over_time = Harvey(G, 1.2)
-    # analyze_cascade(G, G_temp)
-    # plot_cascade(nodes_over_time, "Harvey")
+    G_temp, nodes_over_time = Harvey(G, 1.2)
+    analyze_cascade(G, G_temp)
+    plot_cascade(nodes_over_time, "Harvey")
 
-    # G_temp, nodes_over_time = PolarVortex(G, 1.2)
-    # analyze_cascade(G, G_temp)
-    # plot_cascade(nodes_over_time, "PolarVortex")
+    G_temp, nodes_over_time = PolarVortex(G, 1.2)
+    analyze_cascade(G, G_temp)
+    plot_cascade(nodes_over_time, "PolarVortex")
 
 main()
